2c6b47fe6eddfa34ec2771190c113ecb98a2a691
SC-VRDEMO\ylegat <yessika.legat@dlr.de>
2018-04-03 09:57:06 +0200

    highlight Cubes

---
+++ Assets/Scripts/CubeInteraction.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class CubeInteraction : MonoBehaviour, IFocusable, IInputClickHandler , ISpeechHandler{
+
+    public Color NormalColor;
+    public Color HighlightColor;
+    private Renderer myRenderer;
+    private Material myMaterialInstance;
+    private bool canRotate;
+    private Vector3 initalRotation;
+    private Vector3 updatedRotation;
+
+    #region UnityAPI
+    
+    private void OnValidate()
+    {
+        
+    }
+
+    public void Awake()
+    {
+        myRenderer = gameObject.GetComponent<Renderer>();
+        myMaterialInstance = myRenderer.material;
+       // initalRotation = gameObject.transform.localRotation.eulerAngles;
+    }
+
+    public void OnFocusEnter()
+    {
+       myMaterialInstance.color = HighlightColor;
+    }
+
+    public void OnFocusExit()
+    {
+        myMaterialInstance.color = NormalColor;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (canRotate)
+        {
+            updatedRotation = Vector3.zero;
+            updatedRotation.x = 1;
+            transform.localRotation *= Quaternion.Euler(updatedRotation);
+        }
+		
+	}
+
+    private void OnDestroy()
+    {
+        Destroy(myMaterialInstance);
+    }
+    #endregion
+
+    #region Input
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        StartRotation();
+    }
+
+    public void OnSpeechKeywordRecognized(SpeechEventData eventData)
+    {
+       switch (eventData.RecognizedText.ToLower())
+        {
+            case "Rotate":
+                canRotate = true;
+                break;
+        }
+    }
+
+    #endregion
+
+    public void StartRotation()
+    {
+        canRotate = true;
+    }
+}
