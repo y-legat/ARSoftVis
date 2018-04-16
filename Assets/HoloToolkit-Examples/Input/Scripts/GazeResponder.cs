12ef3cfdf5d788d6c68544a1c1e439f83fbd907d
SC-VRDEMO\ylegat <yessika.legat@dlr.de>
2018-04-16 15:26:10 +0200

    tap to place

--- Assets/HoloToolkit-Examples/Input/Scripts/GazeResponder.cs
+++ Assets/HoloToolkit-Examples/Input/Scripts/GazeResponder.cs
@@ -15,11 +15,14 @@ namespace HoloToolkit.Unity.InputModule.Tests
         private InputManager inputManager;
         private TextMesh textMesh;
         private GameObject text_01;
+        GameObject city;
 
 
         private void Start()
         {
             defaultMaterials = GetComponent<Renderer>().materials;
+            city = GameObject.Find("CityModel");
+
         }
         private void Awake()
         {
@@ -29,28 +32,37 @@ namespace HoloToolkit.Unity.InputModule.Tests
             {
                 inputManager.OverrideFocusedObject = gameObject;
             }
-
-            textMesh = FindObjectOfType<TextMesh>();
-            text_01 = GameObject.Find("InfoText");
+           // GameObject.Find("Canvas").SetActive(true);
+            //GameObject.Find("Canvas/InfoText").SetActive(true);
+            
+            
         }
         public void OnFocusEnter()
         {
-            for (int i = 0; i < defaultMaterials.Length; i++)
+            if (city.activeSelf)
             {
-                // Highlight the material when gaze enters using the shader property.
-                defaultMaterials[i].SetFloat("_Gloss", 70.0f);
-               
-                Debug.Log(gameObject.name);
+                for (int i = 0; i < defaultMaterials.Length; i++)
+                {
+                    // Highlight the material when gaze enters using the shader property.
+                    defaultMaterials[i].SetFloat("_Gloss", 70.0f);
+                    Debug.Log(gameObject.name);
+                }
             }
+            
         }
 
         public void OnFocusExit()
         {
-            for (int i = 0; i < defaultMaterials.Length; i++)
+            if (city.activeSelf)
             {
-                // Remove highlight on material when gaze exits.
-                defaultMaterials[i].SetFloat("_Gloss", 1.0f);
+                for (int i = 0; i < defaultMaterials.Length; i++)
+                {
+                    // Remove highlight on material when gaze exits.
+                    defaultMaterials[i].SetFloat("_Gloss", 1.0f);
+                }
+
             }
+
         }
 
         private void OnDestroy()
@@ -63,17 +75,28 @@ namespace HoloToolkit.Unity.InputModule.Tests
 
         public void OnInputClicked(InputClickedEventData eventData)
         {
-           
-            if (text_01.GetComponent<TextMesh>().text != null && inputManager != null)
-            {
-                // todo visibility text
-               
+            //text_01 = GameObject.Find("InfoText");
+            //Debug.Log(GameObject.Find("InfoText"));
+            //Debug.Log(text_01.GetComponent<TextMesh>());
+            // if (text_01.activeSelf)
 
-                
-                text_01.GetComponent<TextMesh>().text = gameObject.transform.parent.name;
-                inputManager.OverrideFocusedObject = null;
-            }
+            text_01 = GameObject.Find("InfoText");
+            //textMesh = FindObjectOfType<TextMesh>();
+            textMesh = text_01.GetComponent<TextMesh>();
+
+            if (text_01.GetComponent<TextMesh>().text != null && inputManager != null)
+                {
+                    // todo visibility text
+
+
+
+                    text_01.GetComponent<TextMesh>().text = gameObject.transform.parent.name;
+                    inputManager.OverrideFocusedObject = null;
+                }
             
+           
+
         }
+    
     }
 }
---
+++ Assets/Scripts/InitialPlaceByTap.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+namespace LocalJoost.HoloToolkitExtensions
+{
+    public class InitialPlaceByTap : MonoBehaviour
+    {
+        protected AudioSource Sound;
+        protected MoveByGaze GazeMover;
+
+        protected void Start()
+        {
+            Sound = GetComponent<AudioSource>();
+            GazeMover = GetComponent<MoveByGaze>();
+
+            InputManager.Instance.PushFallbackInputHandler(gameObject);
+        }
+
+        public virtual void OnInputClicked(InputEventData eventData)
+        {
+            if (!GazeMover.IsActive)
+            {
+                return;
+            }
+
+            if (Sound != null)
+            {
+                Sound.Play();
+            }
+
+            GazeMover.IsActive = false;
+        }
+    }
+}
