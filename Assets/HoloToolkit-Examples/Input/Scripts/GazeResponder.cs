2180ff575eb45489df4c16fe6bb55d06325043fb
SC-VRDEMO\ylegat <yessika.legat@dlr.de>
2018-04-30 14:12:05 +0200

    city model

--- Assets/HoloToolkit-Examples/Input/Scripts/GazeResponder.cs
+++ Assets/HoloToolkit-Examples/Input/Scripts/GazeResponder.cs
@@ -15,42 +15,62 @@ namespace HoloToolkit.Unity.InputModule.Tests
         private InputManager inputManager;
         private TextMesh textMesh;
         private GameObject text_01;
+        GameObject city;
+
 
 
         private void Start()
         {
-            defaultMaterials = GetComponent<Renderer>().materials;
+            
+            
+            
+
         }
         private void Awake()
         {
+            defaultMaterials = GetComponent<Renderer>().materials;
+            city = GameObject.Find("CityModel");
+            
+            
+            city.SetActive(true);
             inputManager = InputManager.Instance;
+            
 
             if (inputManager != null)
             {
                 inputManager.OverrideFocusedObject = gameObject;
             }
 
-            textMesh = FindObjectOfType<TextMesh>();
-            text_01 = GameObject.Find("InfoText");
+            //textMesh = FindObjectOfType<TextMesh>();
+            //text_01 = GameObject.Find("InfoText");
         }
         public void OnFocusEnter()
         {
-            for (int i = 0; i < defaultMaterials.Length; i++)
+            if (city.activeSelf)
             {
-                // Highlight the material when gaze enters using the shader property.
-                defaultMaterials[i].SetFloat("_Gloss", 70.0f);
-               
-                Debug.Log(gameObject.name);
+                for (int i = 0; i < defaultMaterials.Length; i++)
+                {
+                    // Highlight the material when gaze enters using the shader property.
+                    defaultMaterials[i].SetFloat("_Gloss", 70.0f);
+
+                    Debug.Log(gameObject.name);
+                }
             }
+            
         }
 
         public void OnFocusExit()
         {
-            for (int i = 0; i < defaultMaterials.Length; i++)
+            if (city.activeSelf)
             {
-                // Remove highlight on material when gaze exits.
-                defaultMaterials[i].SetFloat("_Gloss", 1.0f);
+                for (int i = 0; i < defaultMaterials.Length; i++)
+                {
+                    // Remove highlight on material when gaze exits.
+                    defaultMaterials[i].SetFloat("_Gloss", 1.0f);
+                }
+
             }
+            
         }
 
         private void OnDestroy()
@@ -63,13 +83,23 @@ namespace HoloToolkit.Unity.InputModule.Tests
 
         public void OnInputClicked(InputClickedEventData eventData)
         {
-           
+            //text_01 = GameObject.Find("InfoText");
+            //Debug.Log(GameObject.Find("InfoText"));
+            //Debug.Log(text_01.GetComponent<TextMesh>());
+            // if (text_01.activeSelf)
+            //text_01 = GameObject.Find("InfoText");
+            //textMesh = FindObjectOfType<TextMesh>();
+            text_01 = GameObject.Find("InfoText");
+            textMesh = text_01.GetComponent<TextMesh>();
+
+
+
             if (text_01.GetComponent<TextMesh>().text != null && inputManager != null)
             {
                 // todo visibility text
-               
 
                 
+
                 text_01.GetComponent<TextMesh>().text = gameObject.transform.parent.name;
                 inputManager.OverrideFocusedObject = null;
             }
